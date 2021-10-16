c20f5c3ceb8844b511652835b4586967dc3a2f14
YahaouiAnfel <anfel@hotmail.com>
2021-10-16 20:50:39 +0200

    16/10/2021

---
+++ concentrateursDemande/static/js/dataTable.js
@@ -0,0 +1,50 @@
+    $(document).ready(function() {
+        var table1 = $('#dataTable1').DataTable( {
+            "responsive": true,
+            "dom": 'lfrtip',
+            "language": {"url": "https://cdn.datatables.net/plug-ins/1.11.3/i18n/ar.json"},
+            "columnDefs": [ {"targets": [1],"visible": false},
+                            {"targets": [3],"visible": false},
+                            {"targets": [5],"visible": false},
+                            {"targets": [6],"visible": false},
+                            {"targets": [8],"visible": false},
+                            {"targets": [12],"visible": false},
+                           ],
+        } ); 
+
+
+        var table2 = $('#dataTable2').DataTable( {
+
+            "responsive": true,
+            "dom": 'lfrtip',
+            "language": {"url": "https://cdn.datatables.net/plug-ins/1.11.3/i18n/ar.json"},
+            "columnDefs": [ {"targets": [3],"visible": false},
+                            {"targets": [5],"visible": false},
+                            {"targets": [6],"visible": false},
+                            {"targets": [7],"visible": false},
+                            {"targets": [8],"visible": false},
+                            {"targets": [10],"visible": false},
+                           ],
+        } ); 
+
+
+        var table = $('#dataTable').DataTable( {
+            "responsive": true,
+            "language": {"url": "https://cdn.datatables.net/plug-ins/1.11.3/i18n/ar.json"}
+        } ); 
+
+
+        $('a.dropdown-item').on( 'click', function (e) {
+            e.preventDefault();
+            var column = table1.column( $(this).attr('data-column') );
+            column.visible( ! column.visible() );
+        });
+
+
+        $('a.dropdown-item').on( 'click', function (e) {
+            e.preventDefault();
+            var column = table2.column( $(this).attr('data-column') );
+            column.visible( ! column.visible() );
+        }); 
+
+    } );
---
+++ concentrateursDemande/static/js/hover.js
@@ -0,0 +1,47 @@
+function hover1() {
+    element = document.getElementById("button1");
+    var url="/static/assets/img/icone/choix1.png";
+    element.setAttribute('src', url);
+}
+
+function unhover1() {
+    element = document.getElementById("button1");
+    var url="/static/assets/img/icone/choix.png";
+    element.setAttribute('src', url);
+}
+
+
+function hover2() {
+    element = document.getElementById("button2");
+    var url="/static/assets/img/icone/donner1.png";
+    element.setAttribute('src', url);
+}
+function unhover2() {
+    element = document.getElementById("button2");
+    var url="/static/assets/img/icone/donner.png"
+    element.setAttribute('src', url);
+}
+
+
+function hover3() {
+    element = document.getElementById("button3");
+    var url="/static/assets/img/icone/retour1.png";
+    element.setAttribute('src', url);
+}
+function unhover3() {
+    element = document.getElementById("button3");
+    var url="/static/assets/img/icone/retour.png"
+    element.setAttribute('src', url);
+}
+
+
+function hover4() {
+    element = document.getElementById("button4");
+    var url="/static/assets/img/icone/ajouter1.png";
+    element.setAttribute('src', url);
+}
+function unhover4() {
+    element = document.getElementById("button4");
+    var url="/static/assets/img/icone/ajouter.png"
+    element.setAttribute('src', url);
+}
